654772924011d32706abad4ec213ecabec42e1c6
Fernando Alexandre Wahl <fernando.wahl@tbdc.com.br>
2024-06-07 11:46:18 -0300

    key manistes an refactoriing lacation model

--- lib/src/models/location_model.dart
+++ lib/src/models/location_model.dart
@@ -1,6 +1,13 @@
 class LocationModel {
   final double latitude;
   final double longitude;
+  final String? city;
+  final String? country;
 
-  LocationModel({required this.latitude, required this.longitude});
+  LocationModel({
+    required this.latitude,
+    required this.longitude,
+    this.city,
+    this.country,
+  });
 }
