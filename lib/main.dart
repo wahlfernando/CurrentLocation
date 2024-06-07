bdb4946cbdd14b7a1b3febc16bcc7ed10f3bc70d
Fernando Alexandre Wahl <fernando.wahl@tbdc.com.br>
2024-06-07 09:57:34 -0300

    criação do model location_model.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'src/views/map_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
-      home: MapScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Home(),
     );
   }
 }
